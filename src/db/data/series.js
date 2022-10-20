d36f54a3efd6e68f718c5cfad66c718e59633424
Iván Sánchez Corral <ramon.sanchez68@gmail.com>
2022-10-20 16:48:04 +0200

    Added function to get exercise name by series id

--- src/db/data/series.js
+++ src/db/data/series.js
@@ -13,18 +13,21 @@ class Series {
         this.dbConnection.insert('Serie(id_entrenamiento, id_ejercicio)', '(' + idEntrena + ', ' + idExercise + ')');
     }
 
+    async getExerciseName(idSerie){ 
+        return await this.dbConnection.selectWhereAsync('ex_name', 'Serie, Exercise', 'Serie.id = ' + idSerie + " AND Serie.id_ejercicio = Exercise.id");
+    }
+
     async getSeries(idEntrenamiento){
         var results = await this.dbConnection.selectWhereAsync('*', 'Serie', 'id_entrenamiento =  ' + idEntrenamiento + ' ORDER BY id DESC');
         var array = [];
 
         results.forEach(element => {
-            array.push({id: element.id, id_entrenamiento: element.id_entrenamiento, id_ejercicio: element.id_ejercicio})  
+            array.push({id: element.id, id_ejercicio: element.id_ejercicio})  
         });
         return array;
     }
     async getSeriesFull(entrenamientoId){
         var series = await this.getSeries(entrenamientoId)
-        var serieRepe = [];
         for(const serie of series){
 
             var repets = await this.repes.getRepeticiones(serie);
